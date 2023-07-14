d305f9b11cbff6bdce557455abc26657144726a6
ellink <soboleva.doc@gmail.com>
2023-07-14 19:51:13 +0300

    first commit

---
+++ base/js/script.js
@@ -0,0 +1,91 @@
+"use strict";
+
+// let number = 4.6;
+
+// let num = 5;
+// const leftBorderWidth = 1;
+
+
+// const arrObj = {
+//     a: 'a',
+//     1: 'b'
+// }
+
+// arrObj.b = '1234';
+// arrObj['b'] = '1234';
+
+// console.log(arrObj.a);
+// console.log(arrObj[1]);
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+// console.log(arrObj['c']);
+// console.log(arrObj.c);
+
+// num = 10;
+// console.log(num); 
+
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+
+// console.log(obj);
+
+// {
+//     let res =50;
+// }
+
+// console.log(res);
+
+// alert('hello');
+
+// const res = confirm('Are you here?');
+// console.log(res)
+
+// const ans = +prompt('Вам есть 18?', '18'); // динамическая типизация
+// console.log(typeof(ans))
+
+// const ans = [];
+
+// ans[0] = prompt('Name:', 'John');
+// ans[1] = prompt('Surname:', 'Watson');
+// ans[2] = +prompt('Age:', '40');
+
+// document.write(ans);
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Hello, ${user}`)
+
+// console.log('arr' + '- object');
+// console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// incr++;
+// decr--;
+
+// ++incr;
+// --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == 8);
+// console.log(2*4 === '8');
+
+// const isChecked = true,
+//       isClose = false;
+
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+// console.log(isChecked && !isClose);
+
